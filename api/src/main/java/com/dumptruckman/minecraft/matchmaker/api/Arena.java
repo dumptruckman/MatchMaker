dda22f3811170253b1cc11725fe85db2c42e780b
Jeremy Wood <farachan@gmail.com>
2012-03-09 17:37:26 -0500

    Arena no longer extends Region.

--- api/src/main/java/com/dumptruckman/minecraft/matchmaker/api/Arena.java
+++ api/src/main/java/com/dumptruckman/minecraft/matchmaker/api/Arena.java
@@ -2,14 +2,19 @@ package com.dumptruckman.minecraft.matchmaker.api;
 
 import com.dumptruckman.minecraft.matchmaker.api.config.ArenaConfig;
 import com.dumptruckman.minecraft.matchmaker.api.config.ArenaRecord;
+import com.dumptruckman.minecraft.pluginbase.config.Config;
+import com.sk89q.worldedit.BlockVector;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.Vector2D;
 import com.sk89q.worldedit.regions.Region;
 
 import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Represents a single Arena location in the world.
  */
-public interface Arena extends Region {
+public interface Arena {
     
     String getName();
     
@@ -19,7 +24,34 @@ public interface Arena extends Region {
     
     void setMap(ArenaMap map);
     
-    ArenaConfig config();
+    Config config();
     
-    ArenaRecord record();
+    Config record();
+
+    /**
+     * Re-caches the Arena data from persistence.
+     */
+    void refresh();
+
+    public Vector getMinimumPoint();
+
+    public Vector getMaximumPoint();
+
+    public int getArea();
+
+    public int getWidth();
+
+    public int getHeight();
+
+    public int getLength();
+
+    public boolean contains(Vector vector);
+
+    public Set<Vector2D> getChunks();
+
+    public String getWorld();
+
+    public void setWorld(String localWorld);
+
+    public Iterator<BlockVector> iterator();
 }
